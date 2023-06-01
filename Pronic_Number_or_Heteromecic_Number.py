5d311e65f661cefed9883d3c2e05a64b65181cb8
Hanish Jyosyabhatla <129808894+SuryaHanish@users.noreply.github.com>
2023-06-01 18:12:29 +0530

    def pronic(n):     for i in range(1, n):         if i*(i+1)==n:             return True     return False
    num = int(input())
    if pronic(num):
        print("YES")
    else :
        print("NO")

--- Pronic_Number_or_Heteromecic_Number.py
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -1,12 +1,11 @@
-def is_pronic_number(num):
-    for i in range(int(num**0.5) + 1):
-        if i * (i + 1) == num:
+def pronic(n):
+    for i in range(1, n):
+        if i*(i+1)==n:
             return True
     return False
-
-# Example usage
-number = int(input())
-if is_pronic_number(number):
+        
+num = int(input())
+if pronic(num):
     print("YES")
-else:
+else :
     print("NO")
