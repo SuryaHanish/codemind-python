31a8eda31a7b2a938d3e4f69c077f3cba573285b
Hanish Jyosyabhatla <129808894+SuryaHanish@users.noreply.github.com>
2023-06-02 15:20:37 +0530

    def digits(n):     su=0     while n>0:         rem=n%10         su=su+rem         n=n//10     if su>9:         su=digits(su)     return su n=int(input()) x=digits(n) print(x)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,9 +1,12 @@
+def digits(n):
+    su=0
+    while n>0:
+        rem=n%10
+        su=su+rem 
+        n=n//10
+    if su>9:
+        su=digits(su)
+    return su
 n=int(input())
-while(n>9):
-    temp=n
-    s=0
-    while temp:
-        s+=temp%10
-        temp//=10
-    n=s
-print(n)
+x=digits(n)
+print(x)
