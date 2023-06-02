993ca77a601cae6c1ea189936c61b04f65bbebb1
Hanish Jyosyabhatla <129808894+SuryaHanish@users.noreply.github.com>
2023-06-02 15:21:44 +0530

    def perfect_number(n):     su=0     for i in range(1,n):         if n%i==0:             su=su+i     return su==n n=int(input()) if perfect_number(n):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,10 +1,11 @@
+def perfect_number(n):
+    su=0
+    for i in range(1,n):
+        if n%i==0:
+            su=su+i
+    return su==n
 n=int(input())
-s=0
-for i in range(1,n):
-    if(n%i==0):
-        s=s+i
-if(n==s):
+if perfect_number(n):
     print("True")
 else:
     print("False")
-        
